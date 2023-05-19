4e97dfbd9f263ec2fd5baf5de1bddcfcf89f6bef
Anirudh Ravishankar <onirudh3@gmail.com>
2023-05-19 17:19:40 +0200

    New stuff

--- analysis_tree_falls.R
+++ analysis_tree_falls.R
@@ -1,33 +1,67 @@
 # Libraries
 library(survival)
 library(eha)
+library(ggfortify)
+library(survminer)
+library(rms)
+
 
 # Load data
 df_final <- read.csv("treefall_final_table.csv")
 
 ############################ NON-PARAMETRICS ###################################
 # (Kaplan-Meier) Survival function
-with(df_final, plot(Surv(enter, exit, event),
-                main = "Tree Fall (Non-parametric)",
-                xlab = "Age", ylab = "Survival"))
+# with(df_final, plot(Surv(enter, exit, event),
+#                     main = "TREE FALL",
+#                     xlab = "Age", ylab = "Proportion of individuals not experienced risk"))
 
-?Surv()
+help(autoplot.survfit)
+fit <- survfit(Surv(enter, exit, event) ~ 1, data = df_final) # survfit() for survival curves
+autoplot(fit, censor.shape = '|', censor.colour = "orange", surv.colour = "orange") +
+  theme_classic() +
+  ggtitle("TREE FALL") +
+  theme(plot.title = element_text(size = 20)) +
+  xlab("Age in years") +
+  ylab("Proportion of individuals not experienced risk")
+
+# By gender, I'm not able to change the legend title for some reason
+df_final$male <- ifelse(df_final$male == 1, "Male", "Female")
+fit2 <- survfit(Surv(enter, exit, event) ~ male, data = df_final) # survfit() for survival curves
+autoplot(fit2, legTitle = "Gender") +
+  theme_classic() +
+  ggtitle("TREE FALL") +
+  theme(plot.title = element_text(size = 20)) +
+  xlab("Age in years") +
+  ylab("Proportion of individuals not experienced risk")
+
+# Hazard function
+
 
 # (Nelson-Aalen) Cumulative hazards function
-with(df_final, plot(Surv(enter, exit, event), fun = "cumhaz",
-                main = "Tree Fall (Non-parametric)",
-                xlab = "Age", ylab = "Cumulative Hazard"))
+# with(df_final, plot(Surv(enter, exit, event), fun = "cumhaz",
+#                     main = "Tree Fall (Non-parametric)",
+#                     xlab = "Age", ylab = "Cumulative Hazard"))
+
+autoplot(fit, fun = "cumhaz", censor.shape = '|', censor.colour = "orange", surv.colour = "orange") +
+  theme_classic() +
+  ggtitle("TREE FALL") +
+  theme(plot.title = element_text(size = 20)) +
+  xlab("Age in years") +
+  ylab("Cumulative hazard")
+
 
 ################################################################################
 ############################ PARAMETRICS #######################################
 fit.w <- phreg(Surv(enter, exit, event) ~ 1, data = df_final)
 
 # Survival function
-plot(fit.w, fn = "sur", main = "Tree Fall (Parametric)")
+plot(fit.w, fn = "sur", main = "TREE FALL", xlab = "Age in years", ylab = NULL)
+
+# Hazard function
+plot(fit.w, fn = "haz", main = "TREE FALL", xlab = "Age in years", ylab = "Probability of experiencing risk")
 
 # Cumulative hazards (parametric proportional hazards model with no covariates)
-plot(fit.w, fn = "cum", main = "Tree Fall (Parametric)")
-
+plot(fit.w, fn = "cum", main = "TREE FALL", xlab = "Age in years")
 
 ########################## MODELS ##############################################
 # Cox model is h(t) = h0(t) x exp(b1x1 + b2x2 ... + bnxn)
