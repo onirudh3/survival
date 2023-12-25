61fb0fc04748acece3a156220cf74ded2dde746e
Anirudh Ravishankar <onirudh3@gmail.com>
2023-12-25 13:03:04 +0100

    updates december 25

--- time_allocation.R
+++ time_allocation.R
@@ -139,55 +139,14 @@ dz <- subset(dx, verify != 0)
 
 # Tabulate proportions per age category
 prop <- dz %>%
-  group_by(age.cat, sex, activity_group) %>%
+  group_by(age.cat, sex) %>%
   summarise(count = sum(row == "num"), total = n())
 
 prop <- prop %>%
   group_by(age.cat, sex) %>%
   mutate(prop = count / sum(total))
 
-prop <- subset(prop, activity_group != "Other")
-
-
-#### Anderson-Gill ----
-
 # Merge prop with mean hazard file
-prop <- left_join(prop, read.csv("cut_self_mean_hazard_anderson_gill.csv"))
-
-# Make age.cat as factor
-prop$age.cat <- factor(prop$age.cat, levels = c("0-5", "5-10", "10-15", "15-20",
-                                            "20-25", "25-30", "30-35", "35-40",
-                                            "40-45", "45-50", "50-55", "55-60",
-                                            "60+"))
-
-# Plot
-scaleFactor <- max(prop$prop) / max(prop$mean_hazard)
-ggplot(prop, aes(x = age.cat)) +
-  geom_col(aes(y = prop, fill = activity_group), position = "stack", alpha = 0.5, color = 'gray50') +
-  geom_point(aes(y = mean_hazard * scaleFactor), size = 2, color = "blue") +
-  geom_line(aes(y = mean_hazard * scaleFactor), group = 1, size = 1, color = "blue") +
-  theme_classic(base_size = 15) +
-  scale_fill_brewer('', palette = 'Dark2') +
-  labs(x = "Age Category") +
-  scale_y_continuous(name = "Proportion of Time",
-                     sec.axis = sec_axis(~. / scaleFactor, name = "Hazard")) +
-  theme(plot.title = element_text(size = 65, hjust = 0.5),
-        strip.background = element_blank(),
-        strip.placement = 'outside',
-        axis.text.x = element_text(face = 3),
-        panel.grid.major.x = element_blank(),
-        axis.line.y.right = element_line(color = "blue"),
-        axis.ticks.y.right = element_line(color = "blue"),
-        axis.text.y.right = element_text(color = "blue"),
-        axis.title.y.right = element_text(color = "blue")) +
-  ggtitle("Cut Self") +
-  facet_wrap(.~sex, ncol = 1)
-
-
-#### Prentice-Williams[1] ----
-
-# Merge prop with mean hazard file
-prop <- subset(prop, select = -c(mean_hazard))
 prop <- left_join(prop, read.csv("cut_self_mean_hazard_prentice_williams_1.csv"))
 
 # Make age.cat as factor
@@ -199,11 +158,11 @@ prop$age.cat <- factor(prop$age.cat, levels = c("0-5", "5-10", "10-15", "15-20",
 # Plot
 scaleFactor <- max(prop$prop) / max(prop$mean_hazard)
 ggplot(prop, aes(x = age.cat)) +
-  geom_col(aes(y = prop, fill = activity_group), position = "stack", alpha = 0.5, color = 'gray50') +
+  geom_point(aes(y = prop), size = 2) +
+  geom_line(aes(y = prop), group = 1, size = 1) +
   geom_point(aes(y = mean_hazard * scaleFactor), size = 2, color = "blue") +
   geom_line(aes(y = mean_hazard * scaleFactor), group = 1, size = 1, color = "blue") +
   theme_classic(base_size = 15) +
-  scale_fill_brewer('', palette = 'Dark2') +
   labs(x = "Age Category") +
   scale_y_continuous(name = "Proportion of Time",
                      sec.axis = sec_axis(~. / scaleFactor, name = "Hazard")) +
@@ -254,55 +213,14 @@ dz <- subset(dx, verify == 5)
 
 # Tabulate proportions per age category
 prop <- dz %>%
-  group_by(age.cat, sex, activity_group) %>%
+  group_by(age.cat, sex) %>%
   summarise(count = sum(row == "num"), total = n())
 
 prop <- prop %>%
   group_by(age.cat, sex) %>%
   mutate(prop = count / sum(total))
 
-prop <- subset(prop, activity_group != "Other")
-
-
-#### Anderson-Gill ----
-
 # Merge prop with mean hazard file
-prop <- left_join(prop, read.csv("cut_self_mean_hazard_anderson_gill.csv"))
-
-# Make age.cat as factor
-prop$age.cat <- factor(prop$age.cat, levels = c("0-5", "5-10", "10-15", "15-20",
-                                                "20-25", "25-30", "30-35", "35-40",
-                                                "40-45", "45-50", "50-55", "55-60",
-                                                "60+"))
-
-# Plot
-scaleFactor <- max(prop$prop) / max(prop$mean_hazard)
-ggplot(prop, aes(x = age.cat)) +
-  geom_col(aes(y = prop, fill = activity_group), position = "stack", alpha = 0.5, color = 'gray50') +
-  geom_point(aes(y = mean_hazard * scaleFactor), size = 2, color = "blue") +
-  geom_line(aes(y = mean_hazard * scaleFactor), group = 1, size = 1, color = "blue") +
-  theme_classic(base_size = 15) +
-  scale_fill_brewer('', palette = 'Dark2') +
-  labs(x = "Age Category") +
-  scale_y_continuous(name = "Proportion of Time",
-                     sec.axis = sec_axis(~. / scaleFactor, name = "Hazard")) +
-  theme(plot.title = element_text(size = 65, hjust = 0.5),
-        strip.background = element_blank(),
-        strip.placement = 'outside',
-        axis.text.x = element_text(face = 3),
-        panel.grid.major.x = element_blank(),
-        axis.line.y.right = element_line(color = "blue"),
-        axis.ticks.y.right = element_line(color = "blue"),
-        axis.text.y.right = element_text(color = "blue"),
-        axis.title.y.right = element_text(color = "blue")) +
-  ggtitle("Cut Self") +
-  facet_wrap(.~sex, ncol = 1)
-
-
-#### Prentice-Williams[1] ----
-
-# Merge prop with mean hazard file
-prop <- subset(prop, select = -c(mean_hazard))
 prop <- left_join(prop, read.csv("cut_self_mean_hazard_prentice_williams_1.csv"))
 
 # Make age.cat as factor
@@ -314,11 +232,11 @@ prop$age.cat <- factor(prop$age.cat, levels = c("0-5", "5-10", "10-15", "15-20",
 # Plot
 scaleFactor <- max(prop$prop) / max(prop$mean_hazard)
 ggplot(prop, aes(x = age.cat)) +
-  geom_col(aes(y = prop, fill = activity_group), position = "stack", alpha = 0.5, color = 'gray50') +
+  geom_point(aes(y = prop), size = 2) +
+  geom_line(aes(y = prop), group = 1, size = 1) +
   geom_point(aes(y = mean_hazard * scaleFactor), size = 2, color = "blue") +
   geom_line(aes(y = mean_hazard * scaleFactor), group = 1, size = 1, color = "blue") +
   theme_classic(base_size = 15) +
-  scale_fill_brewer('', palette = 'Dark2') +
   labs(x = "Age Category") +
   scale_y_continuous(name = "Proportion of Time",
                      sec.axis = sec_axis(~. / scaleFactor, name = "Hazard")) +
